b6b852df76d37a5b4dd2b915f08168105f171c52
Elizabeth Wells <ewells@Elizabeth-Wells.local>
2018-01-31 13:39:59 -0600

    Initial commit

---
+++ LearningCallback.js
@@ -0,0 +1,27 @@
+function helloCallback(word) {
+    console.log("Hello World"+word)
+}
+function finalCallback() {
+    console.log("Finally, this prints")
+}
+
+function days(callMeBack)  {
+    callMeBack("! It's a good day")
+    callMeBack(". It's a bad day")
+}
+
+function givesACallBack(bob){
+    function foo() {
+
+    }
+}
+
+//does not work because calling setTimeout on "undefined" days() does not return a value
+//setTimeout(days(helloCallback),2500)
+//setTimeout(givesACallBack(helloCallback),2500)
+
+setTimeout(function(){days(helloCallback)}, 2500)
+foo = days(helloCallback)
+console.log(foo)
+console.log("\nThe program keeps going.\n")
+console.log("And going...\n")
